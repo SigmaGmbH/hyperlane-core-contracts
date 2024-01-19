d37bc0607661d50b7f49629a47337391e3379bd9
MikkySnow <mikkysnowman@gmail.com>
2024-01-19 14:45:45 +0100

    remove unused script

--- update_imports.sh
+++
@@ -1,4 +0,0 @@
-# #!/bin/bash
-
-# find types -type f -exec sed -i '' 's|from "ethers";|from "swisstronik-ethers";|g' {} +
-# find types -type f -exec sed -i '' 's|from "@ethersproject/providers";|from "swisstronik-ethers/packages/providers";|g' {} +
